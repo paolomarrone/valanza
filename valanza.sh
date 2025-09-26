0614f0fe10c85925c9ab68efeb4fb0800ffa5a7f
Paolo Marrone <lallero.31@gmail.com>
2025-09-26 18:06:35 +0200

    using tee and /dev/shm

--- valanza.sh
+++ valanza.sh
@@ -11,6 +11,16 @@ if [ ! -f "$DATA_FILE" ]; then
 	exit 1
 fi
 
+RAM_DIR="/dev/shm/$(basename "$0")_$$"  # Use script name + PID for uniqueness
+mkdir -p "$RAM_DIR"
+
+cat "$DATA_FILE" | ./interpolate_lin.R | tee \
+	$RAM_DIR/raw.txt \
+	>(./mov_avg.awk > $RAM_DIR/mov.txt) \
+	>(./lp1.awk > $RAM_DIR/lp1.txt) \
+	> /dev/null
+
+
 gnuplot -persist << EOF
 	set xdata time
 	set timefmt "%Y-%m-%d"
@@ -18,7 +28,7 @@ gnuplot -persist << EOF
 	set xlabel "Date"
 	set ylabel "Weight (kg)"
 	set grid
-	plot '<cat "$DATA_FILE" | ./interpolate_lin.R'                 using 1:2 with lines title "Raw Data", \
-		 '<cat "$DATA_FILE" | ./interpolate_lin.R | ./mov_avg.awk' using 1:2 with lines title "7-Day Moving Average", \
-		 '<cat "$DATA_FILE" | ./interpolate_lin.R | ./lp1.awk'     using 1:2 with lines title "Low-Pass Filter"
+	plot '<cat "$RAM_DIR/raw.txt" ' using 1:2 with lines title "Raw Data", \
+		 '<cat "$RAM_DIR/mov.txt" ' using 1:2 with lines title "7-Day Moving Average", \
+		 '<cat "$RAM_DIR/lp1.txt" ' using 1:2 with lines title "Low-Pass Filter"
 EOF
