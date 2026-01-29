8ec3b3a7aaa85679003b8ddfd59ac251bbf8e025
Paolo Marrone <lallero.31@gmail.com>
2026-01-29 12:43:14 +0100

    make this work on macos

--- valanza.sh
+++ valanza.sh
@@ -8,24 +8,29 @@ if [ ! -f "$DATA_FILE" ]; then
 	exit 1
 fi
 
-RAM_DIR="/dev/shm/$(basename "$0")_$$"
+if [ -d "/dev/shm" ]; then
+	RAM_DIR="/dev/shm/$(basename "$0")_$$"
+else
+	RAM_DIR="/tmp/$(basename "$0")_$$"
+fi
+
 mkdir -p "$RAM_DIR"
 trap 'rm -rf -- "$RAM_DIR"' EXIT
 
-mkfifo $RAM_DIR/dates $RAM_DIR/int $RAM_DIR/avg $RAM_DIR/lp1
+mkfifo "$RAM_DIR/dates" "$RAM_DIR/int" "$RAM_DIR/avg" "$RAM_DIR/lp1"
 
-cat $DATA_FILE | ./interpolate_lin.R | tee \
-	>(cut -d' ' -f1                    > $RAM_DIR/dates) \
-	>(cut -d' ' -f2                    > $RAM_DIR/int) \
-	>(./mov_avg.awk -v col=2 -v win=11 > $RAM_DIR/avg) \
-	>(./lp1.awk     -v col=2           > $RAM_DIR/lp1) \
+cat "$DATA_FILE" | ./interpolate_lin.R | tee \
+	>(cut -d' ' -f1                    > "$RAM_DIR/dates") \
+	>(cut -d' ' -f2                    > "$RAM_DIR/int") \
+	>(./mov_avg.awk -v col=2 -v win=11 > "$RAM_DIR/avg") \
+	>(./lp1.awk     -v col=2           > "$RAM_DIR/lp1") \
 	> /dev/null &
 
 paste -d " " \
-	<(cat $RAM_DIR/dates) \
-	<(cat $RAM_DIR/int) \
-	<(cat $RAM_DIR/avg) \
-	<(cat $RAM_DIR/lp1) \
-	> $RAM_DIR/all.txt
+	<(cat "$RAM_DIR/dates") \
+	<(cat "$RAM_DIR/int") \
+	<(cat "$RAM_DIR/avg") \
+	<(cat "$RAM_DIR/lp1") \
+	> "$RAM_DIR/all.txt"
 
-gnuplot -e "datafile='$RAM_DIR/all.txt'" -persist $PLOT_FILE
+gnuplot -e "datafile='$RAM_DIR/all.txt'" -persist "$PLOT_FILE"
