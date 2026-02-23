1f1304d20a16c7d158de5e2eb045d3c869df20e2
Paolo Marrone <lallero.31@gmail.com>
2026-02-24 00:31:40 +0100

    Make entry scripts path-independent

--- valanza.sh
+++ valanza.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+
 DATA_FILE=$1
 DAYS=${2:-}
-PLOT_FILE="plot.gp"
 
 if [ ! -f "$DATA_FILE" ]; then
 	echo "Error: $DATA_FILE not found"
@@ -26,13 +27,13 @@ trap 'rm -rf -- "$RAM_DIR"' EXIT
 mkfifo "$RAM_DIR/dates" "$RAM_DIR/int" "$RAM_DIR/avg" "$RAM_DIR/lp1"
 
 cat "$DATA_FILE" \
-	| ./interpolate_lin.R \
-	| { [ -n "$DAYS" ] && tail -n "$DAYS" || cat } \
+	| "$SCRIPT_DIR/interpolate_lin.R" \
+	| { [ -n "$DAYS" ] && tail -n "$DAYS" || cat; } \
 	| tee \
-		>(cut -d' ' -f1                    > "$RAM_DIR/dates") \
-		>(cut -d' ' -f2                    > "$RAM_DIR/int")   \
-		>(./mov_avg.awk -v col=2 -v win=11 > "$RAM_DIR/avg")   \
-		>(./lp1.awk     -v col=2           > "$RAM_DIR/lp1")   \
+		>(cut -d' ' -f1                                 > "$RAM_DIR/dates") \
+		>(cut -d' ' -f2                                 > "$RAM_DIR/int")   \
+		>("$SCRIPT_DIR/mov_avg.awk" -v col=2 -v win=11  > "$RAM_DIR/avg")   \
+		>("$SCRIPT_DIR/lp1.awk"     -v col=2            > "$RAM_DIR/lp1")   \
 		> /dev/null &
 
 paste -d " " \
@@ -42,4 +43,4 @@ paste -d " " \
 	<(cat "$RAM_DIR/lp1")   \
 	> "$RAM_DIR/all.txt"
 
-gnuplot -e "datafile='$RAM_DIR/all.txt'" -persist "$PLOT_FILE"
+gnuplot -e "datafile='$RAM_DIR/all.txt'" -persist "$SCRIPT_DIR/plot.gp"
